7c511b87ba2747c2372bd8cc004c26a5a122f0c8
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-02 15:45:24 +0800

    fix bug# `https://www.maikator.tk/map/`网页，不显示`设备分布情况`。 原因是，缺少`marker`图标。放在`项目根目录`的`static`目录，`python manager.py collectstatic`方法，没有将其copy到`staticfiles`目录里面。 解决方法： 1.调整`settings.py`,添加变量`STATICFILES_DIRS`，其内容为：`os.path.join(BASE_DIR,'static')` 2.本以为是编码问题，在文件`map.html`中，添加的utf8编码支持。

--- chinamobile/settings.py
+++ chinamobile/settings.py
@@ -138,17 +138,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR,'static'),
-#)
-
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR,'static'),
-#    ]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 
 # 设置静态文件目录
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
