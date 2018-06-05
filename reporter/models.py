a7637170f11c20711c0cfb34fa2c16dc6410c047
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-05 16:07:16 +0800

    给FiberBox模型，添加publish功能，published_date字段

--- reporter/models.py
+++ reporter/models.py
@@ -2,28 +2,35 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.gis.db import models
-
+from django.utils import timezone
 
 # Create your models here.
 
-class FiberBoxes(models.Model):
-    name = models.CharField(max_length=20,verbose_name='名称')
-    #address = models.CharField(max_length=50,blank=True,verbose_name='地址')
-    village = models.CharField(max_length=50,blank=True,verbose_name='村（社区居委会）')
-    town = models.CharField(max_length=50,blank=True,verbose_name='乡镇（街道办事处）')
-    location = models.PointField(srid=4326,verbose_name='位置')
-    created_at = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
-    updated_at = models.DateTimeField(auto_now=True,verbose_name='更新时间')
-    objects = models.GeoManager()
 
+class FiberBoxes(models.Model):
+    name = models.CharField(max_length=20, verbose_name='名称')
+    #address = models.CharField(max_length=50,blank=True,verbose_name='地址')
+    village = models.CharField(
+        max_length=50, blank=True, verbose_name='村（社区居委会）')
+    town = models.CharField(
+        max_length=50, blank=True, verbose_name='乡镇（街道办事处）')
+    location = models.PointField(srid=4326, verbose_name='位置')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+    objects = models.GeoManager()
 
     def __unicode__(self):
         return self.name
+
     class Meta:
-        verbose_name='光纤箱'
+        verbose_name = '光纤箱'
         verbose_name_plural = "光纤箱"
         #verbose_name_plural = " FiberBoxes"
-        ordering= ("-updated_at",)
+        ordering = ("-updated_at", )
 
 
 class Counties(models.Model):
